2b4f68ef53cc483ce1a6ec4ca9191cd279302de8
zhaoguang <tjuarch@163.com>
2019-03-18 00:43:00 +0800

    风吟鸟唱爬图片
    完成了：
    获取模特展示页面url池
    从页面中提取图片地址
    存储图片。

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 
+
+import threading
+
+def run(n):
+    print("current task：", n)
+
 if __name__ == "__main__":
-    print('hello!')
+    t1 = threading.Thread(target=run, args=("thread 1",))
+    t2 = threading.Thread(target=run, args=("thread 2",))
+    t1.start()
+    t2.start()
--- 风吟鸟唱/niaoz_getimgs.py
+++ 风吟鸟唱/niaoz_getimgs.py
@@ -10,10 +10,98 @@ date：20190316
 from selenium import webdriver
 from time import sleep
 import csv
+import requests
+import os
+import random
+import threading
+
+# 在模特展示页获取大图的地址信息
+def getimgcontent(url, dr):
+    '''
+    url:模特展示页的url，https://niaoz.net/moko/100001361.html
+    dr:webdriver实例
+    实现从当前页获取图片地址信息列表,返回图片地址列表
+    '''
+    res = []
+    try:
+        dr.get(url)
+        # 暂停一下，缓冲缓冲
+        for i in range(3):
+            n = random.random()
+            sleep(n)
+            print(f'wait for {n} seconds')
+        content = dr.find_element_by_id("content")
+        imgs = content.find_elements_by_xpath("//img")
+        for i in imgs:
+            res.append(i.get_attribute('original'))
+        print(res)
+        return res
+    except:
+        with open('errlog.log', 'a') as fp:
+            fp.write(f"{url}, get imgcontent error! \n")
+        return "get imgcontent error"
+
+# 根据url存储图片文件
+def saveimg(url, filepath, imgname):
+    '''
+    实现把地址下载储存为图片
+    '''
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+    try:
+        response = requests.get(url)
+    except:
+        with open('errlog.log', 'a') as fp:
+            fp.write(f"{url}, get img response error! \n")
+        return "get img response error"
+    filename = filepath + os.sep + imgname
+    try:
+        with open(filename, 'wb') as fp:
+            fp.write(response.content)
+        return None
+    except:
+        with open('errlog.log', 'a') as fp:
+            fp.write(f"{url}, save img error! \n")
+        return "save img error"
 
 
-# 打开地址池csv文件，读取一条网址信息
-# 
+def main(modelimgurlfile):
+    dr = webdriver.Firefox()
+    with open(modelimgurlfile, 'r') as fp:
+        num = 0
+        reader = csv.reader(fp)
+        for i in reader:
+            modelimgurl = i[0]
+            print(modelimgurl)
+            imglist = getimgcontent(modelimgurl, dr)
+            for imgurl in imglist:
+                imgname = dr.title.split('|')[0].split(' ')[0] + str(num) + '.' + imgurl.split(".")[-1]
+                num += 1
+                filepath = ".\\img"
+                print(imgname)
+                saveimg(imgurl, filepath, imgname)
+    return "ALL DONE!"
+
+def dividecsv():
+    with open("modelimgurl.csv", 'r', newline='') as fp:
+        reader = csv.reader(fp)
+        num = 0
+        for line in reader:
+            filenum= num // 500
+            filename = "modelimgurl" + str(filenum) + ".csv"
+            with open(filename, 'a', newline='') as fp2:
+                writer = csv.writer(fp2)
+                writer.write(line)
+            num += 1
+    return None
+if __name__ == "__main__":
+    threads = []
+    for i in range(4):
+        t = threading.Thread(target=main, args=(f"modelimgurl{i}.csv",))
+        threads.append(t)
+    for t in threads:
+        t.start()
+    t.join()
 
 
 
