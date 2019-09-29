22390caaec1a9bd821c45f4b76fab103526732a6
zhaoguang <tjuarch@163.com>
2019-09-29 09:47:30 +0800

    20190929

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
+# -*- coding: utf-8 -*-
 
 
-import threading
 
-def run(n):
-    print("current task：", n)
-
-if __name__ == "__main__":
-    t1 = threading.Thread(target=run, args=("thread 1",))
-    t2 = threading.Thread(target=run, args=("thread 2",))
-    t1.start()
-    t2.start()
